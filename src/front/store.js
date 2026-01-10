127626e26b7f6676c1aa0e8abe2757ed3460368f
Asif Alam <104073769+Looperrrrrr@users.noreply.github.com>
2026-01-10 22:14:22 +0000

    localStorage

--- src/front/store.js
+++ src/front/store.js
@@ -3,22 +3,35 @@ export const initialStore = () => {
     message: null,
     products: [],
     cart: [],
+
+    token: localStorage.getItem("token") || null,
+    user_id: localStorage.getItem("user_id") || null,
   };
 };
 
 export default function storeReducer(store, action = {}) {
   switch (action.type) {
     case "set_products":
-      return {
-        ...store,
-        products: action.payload,
-      };
+      return { ...store, products: action.payload };
 
     case "add_to_cart":
+      return { ...store, cart: [...store.cart, action.payload] };
+
+    case "login":
+      localStorage.setItem("token", action.payload.token);
+      localStorage.setItem("user_id", action.payload.user_id);
+
       return {
         ...store,
-        cart: [...store.cart, action.payload],
+        token: action.payload.token,
+        user_id: action.payload.user_id,
       };
+
+    case "logout":
+      localStorage.removeItem("token");
+      localStorage.removeItem("user_id");
+
+      return { ...store, token: null, user_id: null };
 
     default:
       throw Error("Unknown action.");
